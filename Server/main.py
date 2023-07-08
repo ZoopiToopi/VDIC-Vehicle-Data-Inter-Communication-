419458138c23cf14a7b9218b269a2ca73f29f7ef
ZoopiToopi <zoopeetoopee@gmail.com>
2023-07-08 15:20:51 +0530

    pymongo

--- Server/main.py
+++ Server/main.py
@@ -1,9 +1,25 @@
 # Set-ExecutionPolicy Unrestricted -Scope Process
 
-from fastapi import FastAPI
+from  fastapi import FastAPI
+import subprocess
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"server started successfully"}
+run_once = 1
+port = 5050
+ngrok = "ngrok.exe http "+str(port)
+# date = "date"
+while 1:
+    if run_once == 1: 
+        print("starting ngrok server.......")
+        os_data = subprocess.Popen('start cmd /c'+ngrok)
+        print(os_data.decode("utf-8"))
+        run_once = 0
+        
+    # os.system()
+    @app.get("/")
+    async def root():
+        return {"server started successfully"}
+    @app.get("/request/{request_dat}")
+    async def name_func(request_dat):
+        return {"you have sent this to us : "+ request_dat}

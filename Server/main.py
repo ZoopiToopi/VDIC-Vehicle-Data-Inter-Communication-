d33930af2397c6936fdd4c1ab16e9bb8e692fb48
ZoopiToopi <zoopeetoopee@gmail.com>
2023-07-09 15:26:36 +0530

    just organize

--- Server/main.py
+++ Server/main.py
@@ -8,43 +8,23 @@ app = FastAPI()
 # Global Variable
 
 port = 5080
+run_once = 1
+Status = False
 ngrok = "ngrok.exe http "+str(port)
-uvicorn = "uvicorn main:app --reload --port "+str(port)
-
-# Global Variables end
-
-# Private Function Prototypes
 
 def ngrok_Initialize():
     print("\nstarting global server.......")
     subprocess.Popen('start cmd /c'+ngrok,shell = True)
-    return True
-
-def uvicorn_Initialize():
-    print("\nstarting local server.......")
-    subprocess.Popen('start cmd /c'+uvicorn,shell = True)
-    return True
-# Function prototype end
+    # return True
 
 
-
-run_once = 1
-Status = False
 
 # date = "date"
 while 1:
     if run_once == 1: 
-        # Status = uvicorn_Initialize()
-        Status = True
-        # Status = ngrok_Initialize()
+        ngrok_Initialize()
         run_once = 0
-        # if(True == Status):
-        #     Status = False
-        #     Status = ngrok_Initialize()
-        # if(False == Status):
-        #     print("Error! Please check ngrok and uvicorn connection")
-        #     break
-        
+                
     # os.system()
     @app.get("/")
     async def root():
